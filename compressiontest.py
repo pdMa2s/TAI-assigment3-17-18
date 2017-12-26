616f30199779084bd040c7c6171dbdfdf5489ba5
Pedro Matos <pedroguilhermematos@ua.pt>
2017-12-26 23:04:52 +0000

    more compression algorithms

--- compressiontest.py
+++ compressiontest.py
@@ -1,9 +1,36 @@
+import bz2
 import gzip
+import lzma
+import zlib
 
 
-def compress_file():
-    return gzip.compress(open('orl_faces/s01/01.pgm', 'rb').read())
+def compress_file_gzip(content):
+    return gzip.compress(content)
+
+
+def compress_file_bz2(content):
+    return bz2.compress(content)
+
+
+def compress_file_lzma(content):
+    return lzma.compress(content, leve)
+
+
+def compress_file_zlib(content):
+    return zlib.compress(content, level=9)
+
+
+def read_file_content(file_path):
+    return open('orl_faces/'+ file_path, 'rb').read()
 
 
 if __name__ == '__main__':
-    print(len(compress_file()))
+    file_content = read_file_content("s01/01.pgm")
+    compressors = {'gzip': compress_file_gzip,
+                   'bz2': compress_file_bz2,
+                   'lzma': compress_file_lzma,
+                   'zlib': compress_file_zlib}
+    print("original:", len(file_content))
+
+    for c_name, compressor in compressors.items():
+        print("compressor " + c_name + ": " + str(len(compressor(file_content))))
