3bb19adb9dc67154c491c50627d5ea73e6f9d2a9
Andreia Machado <andreiarfmachado4@gmail.com>
2017-12-29 14:31:29 +0000

    image-specific compression

--- ncd.py
+++ ncd.py
@@ -1,31 +1,13 @@
+import numpy as np
+from PIL import Image
+
+
 class NCD:
-    def __init__(self, target_files, ref_files, compressor):
+    def __init__(self, target_files, ref_files, compressor, compressor_type):
         self.target_files = target_files
         self.ref_files = ref_files
         self.compressor = compressor
-        """self.pairs_compress_size = self.generate_pairs()
-        self.array_files_ncd = {}
-        self.calculate_pair_ncd()"""
-
-
-    """def generate_pairs(self):
-        compression = {}
-        for ref in self.ref_files:
-            for tar in self.target_files:
-                reference_content = ref.content
-                target_content = tar.content
-                content = reference_content + target_content
-                compression[(ref, tar)] = len(self.compressor(content))
-        return compression"""
-
-    """def calculate_pair_ncd(self):
-        for x, y in self.pairs_compress_size:
-            compressed_x = x.compress_file_size
-            compressed_y = y.compress_file_size
-
-            compressed_pair = self.pairs_compress_size[(x, y)]
-            ncd = self.ncd(compressed_pair, compressed_x, compressed_y)
-            self.array_files_ncd[(x, y)] = ncd"""
+        self.compressor_type = compressor_type
 
     def mean_ncd(self):
         list_ncd_mean = []
@@ -33,7 +15,11 @@ class NCD:
             y = targ.compress_file_size
             ncd_total = 0
             for ref in self.ref_files:
-                concat_size = len(self.compressor(ref.content + targ.content))
+                concat_size = 0
+                if self.compressor_type == 'jpeg' or self.compressor_type == 'png':
+                    concat_size = self.compressor(self.concate_images(ref.content_image, targ.content_image), self.compressor_type)
+                else:
+                    concat_size = len(self.compressor(ref.content_image + targ.content_image))
                 x = ref.compress_file_size
                 ncd = self.ncd(concat_size,x, y)
                 ncd_total += ncd
@@ -41,8 +27,10 @@ class NCD:
             list_ncd_mean.append((targ, ncd_total))
         return list_ncd_mean
 
-    """def get_array_files_ncd(self):
-        return self.array_files_ncd"""
-
     def ncd(self, concat, x, y):
         return (concat - min(x, y)) / max(x, y)
+
+    def concate_images(self, image1, image2):
+        imgs_comb = np.hstack((np.asarray(i) for i in [image1, image2]))
+        imgs_comb = Image.fromarray(imgs_comb)
+        return imgs_comb
